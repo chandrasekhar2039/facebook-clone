dd865e88a3ae6d9687ffbec0475a24e00983ad4f
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-10-26 19:28:39 +0530

    6

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -44,27 +44,6 @@ function Body(){
     <h1>Body</h1>
     <h1>Body</h1>
     <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
 
 
     </div>
