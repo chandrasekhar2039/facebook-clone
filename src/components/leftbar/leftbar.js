6c2842e956ee140613cdf626e030ad8929aa5278
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-10-26 13:33:21 +0530

    left bar final

--- src/components/leftbar/leftbar.js
+++ src/components/leftbar/leftbar.js
@@ -1,33 +1,42 @@
 import React from "react";
-
+//props
 import Nav from "./props/navigations.js"
-
+//avatar
 import Avatar from '@material-ui/core/Avatar';
-
+//icons
 import FlagTwoToneIcon from '@material-ui/icons/FlagTwoTone';
 import LocalHospitalTwoToneIcon from '@material-ui/icons/LocalHospitalTwoTone';
-import PeopleAltTwoToneIcon from '@material-ui/icons/PeopleAltTwoTone';
+import SupervisorAccountTwoToneIcon from '@material-ui/icons/SupervisorAccountTwoTone';
 import GroupTwoToneIcon from '@material-ui/icons/GroupTwoTone';
 import StorefrontTwoToneIcon from '@material-ui/icons/StorefrontTwoTone';
 import VideoLibraryTwoToneIcon from '@material-ui/icons/VideoLibraryTwoTone';
 import EventAvailableTwoToneIcon from '@material-ui/icons/EventAvailableTwoTone';
 import UpdateTwoToneIcon from '@material-ui/icons/UpdateTwoTone';
 import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
+//css
+import "./style.css";
 
 function Leftbar(){
-  return(<div className="leftbar">
+  return(<div>
+    <div className="leftbar_top">
     <Nav icon={<Avatar />} name="Username" />
     <Nav icon={<FlagTwoToneIcon />} name="Pages" />
     <Nav icon={<LocalHospitalTwoToneIcon />} name="COVID-19 Information Centre" />
-    <Nav icon={<PeopleAltTwoToneIcon />} name="Friends" />
+    <Nav icon={<SupervisorAccountTwoToneIcon />} name="Friends" />
     <Nav icon={<GroupTwoToneIcon />} name="Groups" />
     <Nav icon={<StorefrontTwoToneIcon />} name="Marketplace" />
     <Nav icon={<VideoLibraryTwoToneIcon />} name="Videos" />
     <Nav icon={<EventAvailableTwoToneIcon />} name="Events" />
     <Nav icon={<UpdateTwoToneIcon />} name="Memories" />
     <Nav icon={<KeyboardArrowDownRoundedIcon />} name="See more" />
+  </div>
+ <hr className="line" />
+  <div className="leftbar_botom">
+   <h3>Your Shortcuts</h3>
+    <Nav icon={<GroupTwoToneIcon />} name="VSSUT CLUB" />
+  </div>
 
-  </div>);
+</div>);
 }
 
 export default Leftbar;
