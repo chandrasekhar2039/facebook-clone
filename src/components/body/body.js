76f22d9ae66639d56ad432b69add4f54c7b468bb
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-10-26 19:20:53 +0530

    1

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -88,36 +88,17 @@ function Body(){
     <h1>Body</h1>
     <h1>Body</h1>
     <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
-    <h1>Body</h1>
     
+    <h1>Body</h1>
+    <h1>Body</h1>
+    <h1>Body</h1>
+    <h1>Body</h1>
+    <h1>Body</h1>
+    <h1>Body</h1>
+    <h1>Body</h1>
+    <h1>Body</h1>
+    <h1>Body</h1>
+
 
     </div>
   );
