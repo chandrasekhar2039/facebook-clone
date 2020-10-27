8d413c826b2033b3597fd786c0ef3bc20fb230c0
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-10-27 12:14:51 +0530

    stage-6

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,16 +1,23 @@
 import React from "react";
-
-
+import Stry from "../story/story.js"
+import "./body.css"
+import Createpost from "../create_post/create.js"
 function Body(){
 
 
     return(
-      <div >
-
-    <h1>Body</h1>
-    <p> your components and their props goes here </p>
-    <p>tools and their compostite componets </p>
-
+      <div className="body_whole">
+      <div className="story_frame">
+       <Stry />
+       <Stry />
+       <Stry />
+       <Stry />
+       <Stry />
+       <Stry />
+    </div>
+    <div className="createpost">
+      <Createpost />
+    </div>
     </div>
   );
 }
